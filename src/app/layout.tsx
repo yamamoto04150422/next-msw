37e3c943cf68011c560cb93aca445203035bddb1
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-08-25 20:25:41 +0900

    global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme";
+import "../../styles/global.css";
 
 export default function RootLayout({
   children,
