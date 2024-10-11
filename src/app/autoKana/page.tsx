a3b32a4cb152920885d036330aedc6fd023d5dbe
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-10-11 23:49:53 +0900

    use autokana

--- src/app/autoKana/page.tsx
+++ src/app/autoKana/page.tsx
@@ -1,32 +1,9 @@
 "use client";
 
-import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useAutoKana } from "@/hooks/useAutoKana";
 
 const PersonName = () => {
-  const personNameRef = useRef<HTMLInputElement>(null);
-  const personNameKanaRef = useRef<HTMLInputElement>(null);
-  const [autokana, setAutoKana] = useState<any>(null); // eslint-disable-line
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // クライアントサイドでのみvanilla-autokanaをインポート
-      import("vanilla-autokana").then((AutoKana) => {
-        const ak = AutoKana.bind("#personName", "#personNameKana", {});
-        setAutoKana(ak);
-      });
-    }
-  }, []);
-
-  const onChangeEvent = (e: ChangeEvent<HTMLInputElement>) => {
-    const name = personNameRef.current;
-    const kana = personNameKanaRef.current;
-
-    if (!name) return;
-    name.value = e.target.value;
-
-    if (!kana || !autokana) return;
-    kana.value = autokana.getFurigana();
-  };
+  const { nameRef, nameKanaRef, handleNameChange } = useAutoKana();
 
   return (
     <>
@@ -34,14 +11,14 @@ const PersonName = () => {
         type="text"
         id="personName"
         placeholder="名前"
-        onChange={onChangeEvent}
-        ref={personNameRef}
+        onChange={handleNameChange}
+        ref={nameRef}
       />
       <input
         type="text"
         id="personNameKana"
         placeholder="カナ"
-        ref={personNameKanaRef}
+        ref={nameKanaRef}
       />
     </>
   );
---
+++ src/hooks/useAutoKana.tsx
@@ -0,0 +1,34 @@
+import { useRef, useEffect, useState, ChangeEvent } from "react";
+
+export const useAutoKana = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const nameKanaRef = useRef<HTMLInputElement>(null);
+  const [autokana, setAutoKana] = useState<any>(null); // eslint-disable-line
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // クライアントサイドでのみvanilla-autokanaをインポート
+      import("vanilla-autokana").then((AutoKana) => {
+        const ak = AutoKana.bind("#personName", "#personNameKana", {});
+        setAutoKana(ak);
+      });
+    }
+  }, []);
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = nameRef.current;
+    const kana = nameKanaRef.current;
+
+    if (!name) return;
+    name.value = e.target.value;
+
+    if (!kana || !autokana) return;
+    kana.value = autokana.getFurigana();
+  };
+
+  return {
+    nameRef,
+    nameKanaRef,
+    handleNameChange,
+  };
+};
